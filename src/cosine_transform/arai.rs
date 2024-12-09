7ca3c2c8d06b21d1bca0cca608432d2bb579d63d
Alejandro SM <73649874+sm-alejandro@users.noreply.github.com>
2024-12-09 19:31:41 +0100

    Improve arai adding stride and doing calculation in place

--- src/cosine_transform/arai.rs
+++ src/cosine_transform/arai.rs
@@ -5,61 +5,38 @@ use super::Discrete8x8CosineTransformer;
 pub struct AraiDiscrete8x8CosineTransformer {}
 
 impl Discrete8x8CosineTransformer for AraiDiscrete8x8CosineTransformer {
-    fn transform(values: &[f32; 64]) -> [f32; 64] {
-        let mut row_pass: [f32; 64] = [0.0; 64];
-        let mut counter = 0;
-        for group in values.chunks_exact(8) {
-            let mut row: [f32; 8] = group.try_into().expect("Slice with incorrect length");
-            Self::fast_arai(&mut row);
-            (0..8).for_each(|i| {
-                row_pass[counter] = row[i];
-                counter += 1;
-            });
+    fn transform(values: &mut [f32; 64]) {
+        for i in 0..8 {
+            let start_idx = i * 8;
+            Self::fast_arai(&mut values[start_idx..], 1)
         }
-        let mut column_pass: [f32; 64] = [0.0; 64];
-        for col in 0..8 {
-            let mut group = [0.0; 8];
-            let mut index = [0; 8];
-            for row in 0..8 {
-                let idx = row * 8 + col;
-                group[row] = row_pass[idx];
-                index[row] = idx;
-            }
-            Self::fast_arai(&mut group);
-            for i in 0..8 {
-                column_pass[index[i]] = group[i];
-            }
+        for i in 0..8 {
+            Self::fast_arai(&mut values[i..], 8);
         }
-        column_pass
     }
 }
 
 type Row = [f32; 8];
 
 impl AraiDiscrete8x8CosineTransformer {
-    fn rotate_matrix(input: &mut [f32]) -> Vec<f32> {
-        let mut rotated: Vec<f32> = Vec::new();
-        let mut idx = 0;
-        (0..8).for_each(|i| {
-            idx = i;
-            while idx < input.len() {
-                rotated.push(input[idx]);
-                idx += 8
-            }
-        });
-        log::warn!("len: {}", rotated.len());
-        rotated
-    }
+    fn fast_arai(inputs: &mut [f32], stride: usize) {
+        let v00 = inputs[0];
+        let v01 = inputs[1 * stride];
+        let v02 = inputs[2 * stride];
+        let v03 = inputs[3 * stride];
+        let v04 = inputs[4 * stride];
+        let v05 = inputs[5 * stride];
+        let v06 = inputs[6 * stride];
+        let v07 = inputs[7 * stride];
 
-    fn fast_arai(inputs: &mut Row) {
-        let v10 = inputs[0] + inputs[7];
-        let v11 = inputs[1] + inputs[6];
-        let v12 = inputs[2] + inputs[5];
-        let v13 = inputs[3] + inputs[4];
-        let v14 = inputs[3] - inputs[4];
-        let v15 = inputs[2] - inputs[5];
-        let v16 = inputs[1] - inputs[6];
-        let v17 = inputs[0] - inputs[7];
+        let v10 = v00 + v07;
+        let v11 = v01 + v06;
+        let v12 = v02 + v05;
+        let v13 = v03 + v04;
+        let v14 = v03 - v04;
+        let v15 = v02 - v05;
+        let v16 = v01 - v06;
+        let v17 = v00 - v07;
 
         let v20 = v10 + v13;
         let v21 = v11 + v12;
@@ -74,9 +51,9 @@ impl AraiDiscrete8x8CosineTransformer {
         let v32 = v22 + v23;
 
         let v42 = v32 * Self::A1;
-        let v44 = -v24 * Self::A2 - v24 * Self::A5;
+        let v44 = -v24 * Self::A2 - (v24 + v26) * Self::A5;
         let v45 = v25 * Self::A3;
-        let v46 = v26 * Self::A4 - v26 * Self::A5;
+        let v46 = v26 * Self::A4 - (v26 + v24) * Self::A5;
 
         let v52 = v42 + v23;
         let v53 = v23 - v42;
@@ -88,14 +65,14 @@ impl AraiDiscrete8x8CosineTransformer {
         let v66 = v55 - v46;
         let v67 = v57 - v44;
 
-        inputs[0] = v30 * Self::S0;
-        inputs[4] = v31 * Self::S4;
-        inputs[2] = v52 * Self::S2;
-        inputs[6] = v53 * Self::S6;
-        inputs[5] = v64 * Self::S5;
-        inputs[1] = v65 * Self::S1;
-        inputs[7] = v66 * Self::S7;
-        inputs[3] = v67 * Self::S3;
+        inputs[0 * stride] = v30 * Self::S0;
+        inputs[4 * stride] = v31 * Self::S4;
+        inputs[2 * stride] = v52 * Self::S2;
+        inputs[6 * stride] = v53 * Self::S6;
+        inputs[5 * stride] = v64 * Self::S5;
+        inputs[1 * stride] = v65 * Self::S1;
+        inputs[7 * stride] = v66 * Self::S7;
+        inputs[3 * stride] = v67 * Self::S3;
     }
 
     fn y0(input: &Row) -> f32 {
@@ -122,7 +99,9 @@ impl AraiDiscrete8x8CosineTransformer {
     }
 
     fn y5(i: &Row) -> f32 {
-        (Self::A2 * (i[3] - i[4] + i[2] - i[5]) + Self::A5 * (i[3] - i[4] + i[2] - i[5]) + i[0]
+        (Self::A2 * (i[3] - i[4] + i[2] - i[5])
+            + Self::A5 * (i[3] - i[4] + i[2] - i[5] - i[1] + i[6] - i[0] + i[7])
+            + i[0]
             - i[7]
             - Self::A3 * (i[2] - i[5] + i[1] - i[6]))
             * Self::S_VALUES[5]
@@ -130,14 +109,15 @@ impl AraiDiscrete8x8CosineTransformer {
 
     fn y1(i: &Row) -> f32 {
         let temp = i[1] - i[6] + i[0] - i[7];
-        (i[0] - i[7] + Self::A3 * (i[2] - i[5] + i[1] - i[6]) + Self::A4 * (temp)
-            - Self::A5 * (temp))
+        (i[0] - i[7] + Self::A3 * (i[2] - i[5] + i[1] - i[6]) - Self::A4 * (temp)
+            + Self::A5 * (temp))
             * Self::S_VALUES[1]
     }
 
     fn y7(i: &Row) -> f32 {
         let temp = i[1] - i[6] + i[0] - i[7];
-        (i[0] - i[7] + Self::A3 * (i[2] - i[5] + i[1] - i[6]) - Self::A4 * (temp)
+        (i[0] - i[7] + Self::A3 * (i[2] - i[5] + i[1] - i[6])
+            - Self::A4 * (temp - i[3] + i[4] - i[2] + i[5])
             + Self::A5 * (temp))
             * Self::S_VALUES[7]
     }
@@ -162,13 +142,13 @@ impl AraiDiscrete8x8CosineTransformer {
     const S0: f32 = 0.3535533;
     // From here Sk = 1 / 4*Ck
     // Ck = cos(PI*k/16)
-    const S1: f32 = 0.24519631;
-    const S2: f32 = 0.23096988;
-    const S3: f32 = 0.2078674;
-    const S4: f32 = 0.17677669;
-    const S5: f32 = 0.13889255;
-    const S6: f32 = 0.09567086;
-    const S7: f32 = 0.04877258;
+    const S1: f32 = 0.254_897_8;
+    const S2: f32 = 0.270_598_05;
+    const S3: f32 = 0.300_672_44;
+    const S4: f32 = 0.353_553_38;
+    const S5: f32 = 0.449_988_1;
+    const S6: f32 = 0.653_281_5;
+    const S7: f32 = 1.281_457_7;
 
     #[rustfmt::skip]
     const S_VALUES: &[f32; 8] = &[Self::S0, Self::S1, Self::S2,  Self::S3, Self::S4, Self::S5, Self::S6, Self::S7];
@@ -177,12 +157,14 @@ impl AraiDiscrete8x8CosineTransformer {
 mod test {
     use std::f32::consts::PI;
 
-    use crate::cosine_transform::Discrete8x8CosineTransformer;
+    use crate::cosine_transform::{
+        simple::SimpleDiscrete8x8CosineTransformer, Discrete8x8CosineTransformer,
+    };
 
     use super::AraiDiscrete8x8CosineTransformer;
 
-    fn compute_manually_s(k: u8) -> f32 {
-        f32::cos(PI * k as f32 / 16_f32) * 0.25
+    fn compute_manually_s(k: usize) -> f32 {
+        1.0 / (4.0 * (f32::cos(PI * k as f32 / 16_f32)))
     }
 
     #[rustfmt::skip]
@@ -211,28 +193,60 @@ mod test {
 
     #[test]
     fn test_transform() {
-        let new = AraiDiscrete8x8CosineTransformer::transform(&TEST_VALUES);
+        let mut input = TEST_VALUES.clone();
+        let new = AraiDiscrete8x8CosineTransformer::transform(&mut input);
         log::warn!("{:?}", new)
+    }
+
+    fn assert_eq_with_deviation(actual: f32, expected: f32, deviation: f32, index: usize) {
+        assert!(
+            actual <= expected + deviation,
+            "Value {} at index {} is greater than {} with deviation of {}",
+            actual,
+            index,
+            expected,
+            deviation
+        );
+        assert!(
+            actual >= expected - deviation,
+            "Value {} at index {} is smaller than {} with deviation of {}",
+            actual,
+            index,
+            expected,
+            deviation
+        );
     }
 
     #[test]
     fn test_calculated_s_values() {
-        (1..7).for_each(|i| {
-            assert_eq!(
+        for i in 1..7 {
+            assert_eq_with_deviation(
                 compute_manually_s(i),
-                AraiDiscrete8x8CosineTransformer::S_VALUES[i as usize],
-                "Computed value at index {} is not the same as stored value in constant",
-                i
+                AraiDiscrete8x8CosineTransformer::S_VALUES[i],
+                0.00001,
+                i,
             )
-        });
+        }
+    }
+
+    #[ignore]
+    #[test]
+    fn test_fast_simple() {
+        let mut input: [f32; 64] = [0.0; 64]; // Initialize a mutable array with default values
+        input.copy_from_slice(&TEST_VALUES);
+        AraiDiscrete8x8CosineTransformer::transform(&mut input);
+        let mut input2 = TEST_VALUES.clone();
+        SimpleDiscrete8x8CosineTransformer::transform(&mut input2);
+        for i in 0..64 {
+            assert_eq_with_deviation(input[i], input2[i], 0.001, i)
+        }
     }
 
     #[test]
     fn compare_fast_own() {
-        let mut input: [f32; 8] = [0.0; 8]; // Initialize a mutable array with default values
-        input.copy_from_slice(&TEST_VALUES[0..8]);
-        AraiDiscrete8x8CosineTransformer::fast_arai(&mut input);
-        let mut input2: [f32; 8] = [0.0; 8]; // Initialize a mutable array with default values
+        let mut input = TEST_VALUES.clone();
+        AraiDiscrete8x8CosineTransformer::fast_arai(&mut input, 1);
+        let mut input2: [f32; 8] = [0.0; 8];
         input2.copy_from_slice(&TEST_VALUES[0..8]);
         assert_eq!(
             input[0],
