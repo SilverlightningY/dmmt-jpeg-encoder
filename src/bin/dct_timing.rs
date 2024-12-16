938e7850ccdd64937761863c2a85b31de90f73dd
Florian Korbacher <reband.florian@googlemail.com>
2024-12-16 15:18:44 +0100

    feat: create cli for dct time measurement

--- src/bin/dct_timing.rs
+++ src/bin/dct_timing.rs
@@ -1,7 +1,11 @@
+use std::env::args_os;
+use std::ffi::OsString;
 use std::io::{stdout, Result, Write};
 use std::thread;
 use std::time::{Duration, Instant};
 
+use clap::builder::PossibleValue;
+use clap::{arg, value_parser, Arg, ArgMatches, Command, ValueEnum};
 use dmmt_jpeg_encoder::cosine_transform::{
     arai::AraiDiscrete8x8CosineTransformer, separated::SeparatedDiscrete8x8CosineTransformer,
     simple::SimpleDiscrete8x8CosineTransformer, Discrete8x8CosineTransformer,
@@ -12,6 +16,129 @@ use threadpool::ThreadPool;
 const IMAGE_WIDTH: u16 = 3840;
 const IMAGE_HEIGHT: u16 = 2160;
 const IMAGE_SIZE: usize = IMAGE_WIDTH as usize * IMAGE_HEIGHT as usize;
+
+#[derive(Debug, Clone)]
+enum DCTAlgorithm {
+    Simple,
+    Separated,
+    Arai,
+}
+
+impl ValueEnum for DCTAlgorithm {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::Simple, Self::Separated, Self::Arai]
+    }
+
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        match self {
+            Self::Simple => Some(PossibleValue::new("Simple")),
+            Self::Separated => Some(PossibleValue::new("Separated")),
+            Self::Arai => Some(PossibleValue::new("Arai")),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct CLIParser {
+    command: Command,
+}
+
+impl CLIParser {
+    fn new() -> Self {
+        let command = Self::create_base_command();
+        let command = Self::register_arguments(command);
+        Self { command }
+    }
+
+    fn parse<I, T>(&mut self, itr: I) -> Arguments
+    where
+        I: IntoIterator<Item = T>,
+        T: Into<OsString> + Clone,
+    {
+        let matches = self
+            .command
+            .try_get_matches_from_mut(itr)
+            .unwrap_or_else(|e| e.exit());
+        Self::extract_arguments(&matches)
+    }
+
+    fn create_base_command() -> Command {
+        Command::new("dct_timing")
+    }
+
+    fn register_arguments(command: Command) -> Command {
+        let command = Self::register_threads_argument(command);
+        let command = Self::register_algorithm_argument(command);
+        Self::register_rounds_argument(command)
+    }
+
+    fn register_threads_argument(command: Command) -> Command {
+        command.arg(Self::create_threads_argument())
+    }
+
+    fn register_rounds_argument(command: Command) -> Command {
+        command.arg(Self::create_rounds_argument())
+    }
+
+    fn register_algorithm_argument(command: Command) -> Command {
+        command.arg(Self::crate_algorithm_argument())
+    }
+
+    fn create_rounds_argument() -> Arg {
+        arg!(-r --rounds <ROUNDS> "Number of Rounds")
+            .default_value("1000")
+            .required(false)
+            .value_parser(value_parser!(usize))
+    }
+
+    fn create_threads_argument() -> Arg {
+        arg!(-t --threads <THREADS> "Number of Threads")
+            .default_value(get_number_of_threads().unwrap_or(1).to_string())
+            .required(false)
+            .value_parser(value_parser!(usize))
+    }
+
+    fn crate_algorithm_argument() -> Arg {
+        arg!(-a --algorithm <ALGO> "DCT Algorithm")
+            .default_value("Arai")
+            .value_parser(value_parser!(DCTAlgorithm))
+    }
+
+    fn extract_arguments(matches: &ArgMatches) -> Arguments {
+        Arguments {
+            rounds: Self::extract_rounds_argument(matches),
+            threads: Self::extract_threads_argument(matches),
+            algorithm: Self::extract_algorithm_argument(matches),
+        }
+    }
+
+    fn extract_rounds_argument(matches: &ArgMatches) -> usize {
+        matches
+            .get_one::<usize>("rounds")
+            .expect("Required argument rounds not privided")
+            .to_owned()
+    }
+
+    fn extract_threads_argument(matches: &ArgMatches) -> usize {
+        matches
+            .get_one::<usize>("threads")
+            .expect("Required argument threads not provided")
+            .to_owned()
+    }
+
+    fn extract_algorithm_argument(matches: &ArgMatches) -> DCTAlgorithm {
+        matches
+            .get_one::<DCTAlgorithm>("algorithm")
+            .expect("Required argument algorithm not provided")
+            .to_owned()
+    }
+}
+
+struct Arguments {
+    rounds: usize,
+    threads: usize,
+    algorithm: DCTAlgorithm,
+}
 
 struct Measurement {
     durations: Vec<Duration>,
@@ -73,7 +200,7 @@ fn transform_channel(
     let start = Instant::now();
     unsafe {
         let channel_ptr = &raw mut channel[0];
-        transformer.transform_on_threadpool(threadpool, channel_ptr, channel.len(), 200);
+        transformer.transform_on_threadpool(threadpool, channel_ptr, channel.len(), 700);
     }
     threadpool.join();
     start.elapsed()
@@ -159,14 +286,27 @@ fn get_number_of_threads() -> Result<usize> {
 }
 
 fn main() {
+    let mut cli_parser = CLIParser::new();
+    let arguments = cli_parser.parse(args_os());
+    let number_of_rounds = arguments.rounds;
+    let number_of_threads = arguments.threads;
+
     println!("Creating test image");
     let test_image = create_test_image();
     let blocks = cut_image_into_blocks(&test_image);
     let channel = blocks.into_flattened();
-    let number_of_threads = get_number_of_threads().unwrap_or(4);
+    println!("Creating Threadpool with {} threads", number_of_threads);
     let threadpool = ThreadPool::new(number_of_threads);
 
-    run_simple_algorithm_measurement(&channel, 5, &threadpool);
-    run_separated_algorithm_measurement(&channel, 140, &threadpool);
-    run_arai_algorithm_measurement(&channel, 160, &threadpool);
+    match arguments.algorithm {
+        DCTAlgorithm::Simple => {
+            run_simple_algorithm_measurement(&channel, number_of_rounds, &threadpool);
+        }
+        DCTAlgorithm::Separated => {
+            run_separated_algorithm_measurement(&channel, number_of_rounds, &threadpool);
+        }
+        DCTAlgorithm::Arai => {
+            run_arai_algorithm_measurement(&channel, number_of_rounds, &threadpool);
+        }
+    }
 }
